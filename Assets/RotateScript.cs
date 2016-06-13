09a5b488f80677244b83d7bcb9e0307d4ce0d081
Nick Santaniello <nicksantan@gmail.com>
2016-06-13 01:24:45 -0400

    Added small explosion, tweaked powerup model, added rotate script.

---
+++ Assets/RotateScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(new Vector3(.5f, 2f, .5f) * Time.deltaTime * 100f);
+	}
+}
