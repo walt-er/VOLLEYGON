66883703f388411447a00cbf8f8b732032a293f2
Nick Santaniello <nicksantan@gmail.com>
2016-07-22 00:59:28 -0400

    Messing around with unity's crappy movie textures

--- Assets/Scripts/PlayMovieScript.cs
+++ Assets/Scripts/PlayMovieScript.cs
@@ -2,15 +2,26 @@
 using System.Collections;
 
 public class PlayMovieScript : MonoBehaviour {
-
+	public MovieTexture movTexture;
+	private bool fadedOut;
 	// Use this for initialization
 	void Start () {
+		fadedOut = false;
 		// this line of code will make the Movie Texture begin playing
-		((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+		//((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+		GetComponent<Renderer>().material.mainTexture = movTexture;
+		movTexture.Play();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (!movTexture.isPlaying && !fadedOut) {
+			
+			fadedOut = true;
+			iTween.FadeTo (gameObject, 0f, .5f);
+		}
+			
 	
 	}
 }
