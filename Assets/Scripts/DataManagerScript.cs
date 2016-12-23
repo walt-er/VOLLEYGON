5ad0c6e7724e14c512684f1531a1907373452161
Nick Santaniello <nicksantan@gmail.com>
2016-12-22 21:15:54 -0800

    Bumped version number

--- Assets/Scripts/DataManagerScript.cs
+++ Assets/Scripts/DataManagerScript.cs
@@ -65,7 +65,7 @@ public class DataManagerScript : MonoBehaviour {
 	}
 	// Use this for initialization
 	void Start () {
-		version = "V1.4.1";
+		version = "V1.4.2";
 	}
 	
 	// Update is called once per frame
