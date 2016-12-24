17a8d5a112cec4d3212bf5cc2352ca37259e0428
Nick Santaniello <nicksantan@gmail.com>
2016-12-23 17:44:07 -0800

    Added new replacement for rocky arena

--- Assets/Scripts/middleNetScript.cs
+++ Assets/Scripts/middleNetScript.cs
@@ -4,20 +4,22 @@ using System.Collections;
 public class middleNetScript : MonoBehaviour {
 	private Rigidbody2D rb;
 	public float speed = 1000.0f;
+	public float borderPosition;
+	public float directionModifier;
 	// Use this for initialization
 	void Start () {
 		rb = gameObject.GetComponent<Rigidbody2D>();
 
-		rb.velocity = new Vector2 (0, speed);
+		rb.velocity = new Vector2 (0, directionModifier * speed);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (transform.localPosition.y >= 6.5) {
+		if (transform.localPosition.y >= borderPosition) {
 			rb.velocity = new Vector2 (0, speed * -1);
 		}
 
-		if (transform.localPosition.y <= -6.5) {
+		if (transform.localPosition.y <= -1 * borderPosition) {
 			rb.velocity = new Vector2 (0, speed * 1);
 		}
 	}
