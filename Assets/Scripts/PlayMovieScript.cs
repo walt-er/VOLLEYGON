6a91dcca6a4d6e78dd5457d17d0dd4f609b3ff0f
Nick Santaniello <nicksantan@gmail.com>
2017-07-30 22:54:28 -0400

    Cleaned up title scene

--- Assets/Scripts/PlayMovieScript.cs
+++ Assets/Scripts/PlayMovieScript.cs
@@ -3,9 +3,11 @@ using System.Collections;
 using UnityEngine.Video;
 
 public class PlayMovieScript : MonoBehaviour {
-	public MovieTexture movTexture;
-	public VideoPlayer vidPlayer;
+	private VideoPlayer vidPlayer;
 	public GameObject pressStartAnimation;
+	public GameObject stillLogo;
+	public GameObject colorCyclingBG;
+
 	private bool fadedOut;
 
 	void Start () {
@@ -13,22 +15,32 @@ public class PlayMovieScript : MonoBehaviour {
 		vidPlayer = GetComponent<VideoPlayer> ();
 		//GetComponent<Renderer>().material.mainTexture = movTexture;
 		//movTexture.Play();
+		Invoke("FadeOutVideo", 8f);
+		Invoke ("StartColorCycling", 1f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (!fadedOut && !vidPlayer.isPlaying) {
-			
-			fadedOut = true;
-			iTween.FadeTo (gameObject, 0f, .5f);
-			Invoke ("FireAnimation", 1f);
-			//gameObject.SetActive(false);
-		}
-			
-	
+//		if (!fadedOut && !vidPlayer.isPlaying) {
+//			
+//			fadedOut = true;
+//			iTween.FadeTo (gameObject, 0f, .5f);
+//			Invoke ("FireAnimation", 1f);
+//			//gameObject.SetActive(false);
+//		}
 	}
 
+	void StartColorCycling(){
+		colorCyclingBG.SetActive (true);
+	}
+
+	void FadeOutVideo(){
+		stillLogo.SetActive (true);
+
+		iTween.FadeTo (gameObject, 0f, .5f);
+		Invoke ("FireAnimation", 1f);
+	}
 	void FireAnimation(){
 		pressStartAnimation.GetComponent<PlayAnimationScript> ().PlayAnimation ();
 	}
