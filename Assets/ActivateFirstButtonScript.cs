8d238c8987a2543e283e7311c04494b4da4f4a43
Nick Santaniello <nicksantan@gmail.com>
2017-08-13 19:19:41 -0400

    Building out basic logic for menu systems

---
+++ Assets/ActivateFirstButtonScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ActivateFirstButtonScript : MonoBehaviour {
+
+	public EventSystem eventSystem;
+	public GameObject selectedObject;
+
+	// Use this for initialization
+	void Start () {
+		eventSystem.SetSelectedGameObject (selectedObject);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+	}
+}
---
+++ Assets/ChangeButtonTextColorScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ChangeButtonTextColorScript : MonoBehaviour, ISelectHandler {
+
+	public Text t;
+
+	public void ChangeColor(Color whichColor)
+	{
+		t.color = whichColor;
+	}
+//
+
+	public void OnDeselect(BaseEventData eventData){
+		ChangeColor (Color.green);
+		Debug.Log ("DISABLED!");
+	}
+	public void OnSelect(BaseEventData eventData){
+		ChangeColor (Color.blue);
+	}
+
+
+}
+
+
