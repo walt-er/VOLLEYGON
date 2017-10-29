e5fc77ea0086ad6ab1325adfadaac2816d2ce9eb
Nick Santaniello <nicksantan@gmail.com>
2017-10-29 19:11:40 -0400

    Built basic options manager

---
+++ Assets/OptionsManagerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsManagerScript : MonoBehaviour {
+
+	private int whichPlayerIsControlling;
+	private JoystickButtons joyButts;
+	// Use this for initialization
+	void Start () {
+		
+		// determine which controller is 'in control'. For now, let's just say player one
+		whichPlayerIsControlling = 1;
+		joyButts = new JoystickButtons (1);
+		//TODO: Will need to handle controller disconnects, etc.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown (joyButts.grav)) {
+			Application.LoadLevel ("titleScene");
+		}
+
+		// Check for cancel button to go to previous scene
+				
+	}
+}
