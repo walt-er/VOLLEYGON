78fda0149042d73fb71cadd31e4a65ac795e34c9
Walt Mitchell <walt@walt.codes>
2017-12-16 18:32:14 -0500

    testing User functions, log in on title screen start

--- Assets/Scripts/UserUtility.cs
+++ Assets/Scripts/UserUtility.cs
@@ -5,13 +5,57 @@ using System;
 using Users;
 
 public class UserUtility : MonoBehaviour {
+	SignOutDeferral deferral;
+
 	void Start() {
 		if (Application.platform == RuntimePlatform.XboxOne) {
 
 			UsersManager.Create();
-			User user = UsersManager.GetAppCurrentUser();
-			Debug.Log(user);
+
+			UsersManager.OnUsersChanged          += OnUsersChanged;
+			UsersManager.OnUserSignIn            += OnUserSignIn;
+			UsersManager.OnUserSignOut           += OnUserSignOut;
+			UsersManager.OnSignOutStarted        += OnUserSignOutStarted;
+			UsersManager.OnDisplayInfoChanged    += OnUserDisplayInfoChange;
+			UsersManager.OnAppCurrentUserChanged += OnAppCurrentUserChange;
 
 		}
+	}
+
+	void OnUsersChanged(int id,bool wasAdded)
+	{
+		Debug.Log("> Users List Changed ["+id.ToString()+"] ["+(wasAdded?"Added":"Removed")+"]\n");
+	}
+
+	void OnUserSignIn(int id)
+	{
+		Debug.Log("> User Signed In: [" + id.ToString() + "]\n");
+	}
+
+	void OnUserSignOut(int id)
+	{
+		Debug.Log("> User Signed Out: [" + id.ToString() + "]\n");
+	}
+
+	void OnUserSignOutStarted(int id, IntPtr deferred)
+	{
+		Debug.Log("> User Signed Out Started: [" + id.ToString() + "]\n");
+		deferral = new SignOutDeferral(deferred);
+	}
+
+	void OnUserDisplayInfoChange(int id)
+	{
+		Debug.Log("> User Display Info Changed: [" + id.ToString() + "]\n");
+	}
+
+	void OnAppCurrentUserChange()
+	{
+		User u = UsersManager.GetAppCurrentUser();
+		string info = "no App Current User";
+		if ( null != u )
+		{
+			info = u.Id + ": " + u.OnlineID;
+		}
+		Debug.Log("> App Current User Changed: [" + info + "]\n");
 	}
 }
