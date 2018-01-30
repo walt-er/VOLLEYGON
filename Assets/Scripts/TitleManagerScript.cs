6da2913457f143eddcca3bf0e2c558f3bc8550bf
Walt Mitchell <walt@walt.codes>
2018-01-29 20:15:36 -0500

    remove Xbox-specific functionality

--- Assets/Scripts/TitleManagerScript.cs
+++ Assets/Scripts/TitleManagerScript.cs
@@ -40,100 +40,23 @@ public class TitleManagerScript : MonoBehaviour {
 		//Cursor.lockState = CursorLockMode.Locked;
 	}
 
-	public static readonly XboxOneKeyCode[] startTitleButtons = {
-		XboxOneKeyCode.Gamepad1ButtonA,
-		XboxOneKeyCode.Gamepad2ButtonA,
-		XboxOneKeyCode.Gamepad3ButtonA,
-		XboxOneKeyCode.Gamepad4ButtonA,
-		XboxOneKeyCode.Gamepad1ButtonX,
-		XboxOneKeyCode.Gamepad2ButtonX,
-		XboxOneKeyCode.Gamepad3ButtonX,
-		XboxOneKeyCode.Gamepad4ButtonX,
-		XboxOneKeyCode.Gamepad1ButtonView,
-		XboxOneKeyCode.Gamepad2ButtonView,
-		XboxOneKeyCode.Gamepad3ButtonView,
-		XboxOneKeyCode.Gamepad4ButtonView,
-		XboxOneKeyCode.Gamepad1ButtonMenu,
-		XboxOneKeyCode.Gamepad2ButtonMenu,
-		XboxOneKeyCode.Gamepad3ButtonMenu,
-		XboxOneKeyCode.Gamepad4ButtonMenu
-	};
-
-	public static readonly XboxOneKeyCode[] yButtons = {
-		XboxOneKeyCode.Gamepad1ButtonY,
-		XboxOneKeyCode.Gamepad2ButtonY,
-		XboxOneKeyCode.Gamepad3ButtonY,
-		XboxOneKeyCode.Gamepad4ButtonY
-	};
-
-    public static bool GetValidButtonDown(XboxOneKeyCode[] validButtons, out XboxOneKeyCode buttonThatWasPressed) {
-        for (int i = 0; i < validButtons.Length; i++) {
-            XboxOneKeyCode keyCode = validButtons[i];
-            if (XboxOneInput.GetKeyDown(keyCode)) {
-                buttonThatWasPressed = keyCode;
-                return true;
-            }
-        }
-        buttonThatWasPressed = XboxOneKeyCode.Gamepad1ButtonA;
-        return false;
-    }
-
 	void Update () {
 		MusicManagerScript.Instance.FadeOutEverything ();
 
 		// Listen for activation
 		if (!mainMenuActive) {
-			// Look for xbox input
-			if (XboxOneInput.GetKeyDown( XboxOneKeyCode.GamepadButtonA )
-				|| XboxOneInput.GetKeyDown( XboxOneKeyCode.GamepadButtonX )
-				|| XboxOneInput.GetKeyDown( XboxOneKeyCode.GamepadButtonView )
-				|| XboxOneInput.GetKeyDown( XboxOneKeyCode.GamepadButtonMenu )
-				) {
+			// Iterate over all inputs for actions
+			for (int i = 0; i < gamepads.Length; i++) {
+				if (Input.GetButtonDown (gamepads[i].jump) || Input.GetButtonDown (gamepads[i].start)) {
 
-				// Get gamepad
-	            XboxOneKeyCode keyCode;
-	            if (GetValidButtonDown(startTitleButtons, out keyCode)) {
-	                int gamepadIndex = (int)XboxOneInput.GetGamepadIndexFromGamepadButton(keyCode);
+					// Xbox numbering
+					int gamepadIndex = i + 1;
 
-	                // Sign in if active player is not associated with this controller
-	                if (XboxOneInput.GetUserIdForGamepad((uint)gamepadIndex) == 0) {
-	                	DataManagerScript.shouldActivateMenu = true;
-						UsersManager.RequestSignIn(Users.AccountPickerOptions.None, (ulong)gamepadIndex);
-					}
-					else {
-	 					// Open main menu with this controller
-						activateMainMenu(gamepadIndex);
-					}
-				}
-			}
-			else {
-				// Iterate over all inputs for actions
-				for (int i = 0; i < gamepads.Length; i++) {
-					if (Input.GetButtonDown (gamepads[i].jump) || Input.GetButtonDown (gamepads[i].start)) {
-
-	 					// Open main menu
-						activateMainMenu(i + 1);
-					}
+					// Open main menu
+					activateMainMenu(gamepadIndex);
 				}
 			}
 		} else {
-			// Listen for user change (Y button)
-			if (XboxOneInput.GetKeyDown( XboxOneKeyCode.GamepadButtonY )) {
-
-				// Get gamepad
-	            XboxOneKeyCode keyCode;
-	            if (GetValidButtonDown(yButtons, out keyCode)) {
-	                int gamepadIndex = (int)XboxOneInput.GetGamepadIndexFromGamepadButton(keyCode);
-
-	                // Back out and log in again if active player presses Y
-	                if (gamepadIndex == DataManagerScript.gamepadControllingMenus) {
-						cancelCurrentMenu(true);
-	                	DataManagerScript.shouldActivateMenu = true;
-						UsersManager.RequestSignIn(Users.AccountPickerOptions.None, (ulong)gamepadIndex);
-					}
-				}
-			}
-
 			// Listen for cancel
 			if (controllingGamepad != null &&  Input.GetButtonDown (controllingGamepad.grav)) {
 				cancelCurrentMenu(false);
@@ -160,12 +83,6 @@ public class TitleManagerScript : MonoBehaviour {
 
 		// Addign gamepad to menus
 		DataManagerScript.gamepadControllingMenus = gamepad;
-
-		// Save "active" user if on xbox
-		if (DataManagerScript.xboxMode) {
-			int userId = XboxOneInput.GetUserIdForGamepad((uint)gamepad);
-			DataManagerScript.userControllingMenus = UsersManager.FindUserById(userId);
-		}
 
 		// activate menu and its first button (weird ui thing)
 		mainMenuActive = true;
