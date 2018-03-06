8e027eec96d4143f09231f53de169f15cf8a0b37
Nick Santaniello <nicksantan@gmail.com>
2018-03-05 21:55:56 -0500

    Building out multiball challenge

--- Assets/IndividualChallengeManagerScript.cs
+++ Assets/IndividualChallengeManagerScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class IndividualChallengeManagerScript : MonoBehaviour {
 
@@ -13,18 +14,19 @@ public class IndividualChallengeManagerScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		ballPrefab = ChallengeManagerScript.Instance.GetComponent<ChallengeManagerScript>().ballPrefab;
-		GameObject ball_1 = Instantiate(ballPrefab, new Vector3(3, 0, 0), Quaternion.identity);
-		GameObject ball_2 = Instantiate(ballPrefab, new Vector3(1, 2, 0), Quaternion.identity);
-		Debug.Log ("trying to launch");
-
-		IEnumerator coroutine_1 = ball_1.GetComponent<BallScript> ().LaunchBallWithDelay (1f, -3f, -2f);
-		StartCoroutine(coroutine_1);
-
-		IEnumerator coroutine_2 = ball_2.GetComponent<BallScript> ().LaunchBallWithDelay (1f, -3f, -2f);
-		StartCoroutine(coroutine_2);
+//		GameObject ball_1 = Instantiate(ballPrefab, new Vector3(3, 0, 0), Quaternion.identity);
+//		GameObject ball_2 = Instantiate(ballPrefab, new Vector3(1, 2, 0), Quaternion.identity);
+//		Debug.Log ("trying to launch");
+//
+//		IEnumerator coroutine_1 = ball_1.GetComponent<BallScript> ().LaunchBallWithDelay (1f, -3f, -2f);
+//		StartCoroutine(coroutine_1);
+//
+//		IEnumerator coroutine_2 = ball_2.GetComponent<BallScript> ().LaunchBallWithDelay (1f, -3f, -2f);
+//		StartCoroutine(coroutine_2);
 
 //		ball_1.GetComponent<BallScript> ().LaunchBallWithDelay (1f, -3f, -2f);
 //		ball_2.GetComponent<BallScript> ().LaunchBallWithDelay (1.5f, -6f, 0f);
+		StartCoroutine(LaunchBalls(2f, 5));
 	}
 	
 	// Update is called once per frame
@@ -36,4 +38,23 @@ public class IndividualChallengeManagerScript : MonoBehaviour {
 		Debug.Log ("the ball has died");
 		//ChallengeManagerScript.Instance.ChallengeFail ();
 	}
+
+	public void LaunchBall(float x, float y, float z){
+		GameObject ball_1 = Instantiate(ballPrefab, new Vector3(x, y, z), Quaternion.identity);
+		IEnumerator coroutine_1 = ball_1.GetComponent<BallScript> ().LaunchBallWithDelay (0f, -6f, -10f);
+		StartCoroutine(coroutine_1);
+
+
+	}
+
+	public IEnumerator LaunchBalls(float interval, int invokeCount)
+	{
+		for (int i = 0; i < invokeCount; i++)
+		{
+			LaunchBall(UnityEngine.Random.Range(0, 3f),UnityEngine.Random.Range(-1, 2f),0f);
+
+			yield return new WaitForSeconds(interval);
+		}
+	}
 }
+
