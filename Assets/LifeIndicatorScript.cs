a0074b36c3261195ca071b86100327522fd30fe3
Nick Santaniello <nick@robysoft.net>
2019-04-02 15:55:52 -0700

    Added first pass of a life indicator prefab

---
+++ Assets/LifeIndicatorScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeIndicatorScript : MonoBehaviour
+{
+
+    public GameObject targetPlayer;
+    public int playerType = -1; 
+
+    void Start()
+    {
+        // Identify target player by tag.
+        Debug.Log("Life indicator looking for player");
+        targetPlayer = GameObject.FindWithTag("Player");
+        if (targetPlayer != null)
+        {
+            Debug.Log("Target player found");
+            Debug.Log(targetPlayer);
+            playerType = targetPlayer.GetComponent<PlayerController>().playerType;
+            Debug.Log("player type is " + playerType);
+        }
+        else
+        {
+            Debug.Log("Player not found");
+        }
+
+        if (playerType != -1)
+        {
+            this.gameObject.transform.GetChild(playerType).gameObject.SetActive(true);
+        }
+    }
+
+
+    void Update()
+    {
+        // Update the rotation to match the player's rotation
+    }
+}
---
+++ Assets/LivesManagerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivesManagerScript : MonoBehaviour
+{
+    public int lives = 3;
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    public void onBallDied()
+    {
+        lives -= 1;
+        if (lives < 0)
+        {
+            //broadcast gameover
+        }
+    }
+}
